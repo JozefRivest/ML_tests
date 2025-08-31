280827d37861c3e85aa16058cc7f8d2c7785cc5c
JozefRivest <jrivest@umich.edu>
2025-08-31 12:13:44 -0400

    Few clarifications

--- ridge_lasso.R
+++ ridge_lasso.R
@@ -1,3 +1,9 @@
+# ============== Simulation 1 ================
+
+##
+# Here, y is function of two independent variables
+##
+
 n <- 10000
 
 v <- rnorm(n, mean = 0, sd = 1)
@@ -6,6 +12,39 @@ z <- rnorm(n, mean = 0, sd = 1)
 
 x <- 2 * z + rnorm(n, mean = 0, sd = 1)
 y <- 3 * x + 2 * v + rnorm(n, mean = 0, sd = 1)
+
+data <- dplyr::bind_cols(y, x, z, v, w)
+
+cor(data)
+
+mod <- lm(y ~ x + z + w + v, data = data)
+summary(mod)
+
+grid <- 10^seq(10, -2, length = 100)
+
+predictors <- dplyr::bind_cols(v, w, x, z)
+
+ridge <- glmnet::glmnet(predictors, y, lambda = grid, alpha = 0)
+lasso <- glmnet::glmnet(predictors, y, lambda = grid, alpha = 1)
+
+plot(ridge)
+plot(lasso)
+
+# ============== Simulation 2 ================
+
+##
+# Here, y is function of two x and z, where the latter
+# creates a backdoor bias
+##
+
+n <- 10000
+
+v <- rnorm(n, mean = 0, sd = 1)
+w <- rnorm(n, mean = 0, sd = 1)
+z <- rnorm(n, mean = 0, sd = 1)
+
+x <- 2 * z + rnorm(n, mean = 0, sd = 1)
+y <- 3 * x + 2 * z + rnorm(n, mean = 0, sd = 1)
 
 data <- dplyr::bind_cols(y, x, z, v, w)
 
